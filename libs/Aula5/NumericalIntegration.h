e0ab32b5a40016e870f920473ceea7c08a06bb56
Albertini96 <guilhermeoliveira@arttu.co>
2019-09-16 01:00:58 -0300

    Monte Carlo finished

--- libs/Aula5/NumericalIntegration.h
+++ libs/Aula5/NumericalIntegration.h
@@ -17,73 +17,90 @@ private:
     
 public:
     
+    //Calculates the factorial of n
     int factorial(int n){
         return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
     }
     
+    //Calculate the combinatory of n and k
     int comb(int n, int k){
         return (factorial(n))/(factorial(k) * factorial(n - k));
     }
     
+    //Evaluate function 1 on point x
     float f1(float x){
         return exp(x);
     }
     
+    //Evaluate function 2 on point x
     float f2(float x){
         return sqrt(1 - pow(x, 2));
     }
     
+    //Evaluate function 3 on point x
     float f3(float x){
         return exp(-pow(x, 2));
     }
     
     //F1
+    //Calculate the defined integral from _initial to _final for function 1 using MidPoint method
     float midPointIntegrationF1(float _initial, float _final){
         return (_final - _initial) * (f1((_final + _initial) / 2));
     }
     
+    //Calculate the defined integral from _initial to _final for function 1 using Trapezoidal method
     float trapezoidalIntegrationF1(float _initial, float _final){
         return (_final - _initial) * ((f1(_initial) + f1(_final)) / 2);
     }
     
+    //Calculate the defined integral from _initial to _final for function 1 using Simpson method
     float simpsonIntegrationF1(float _initial, float _final){
         return (_final - _initial) * ((f1(_initial) + (4* (f1((_final + _initial) / 2)) ) +  f1(_final)) / 6);
     }
     
     //F2
+    //Calculate the defined integral from _initial to _final for function 2 using MidPoint method
     float midPointIntegrationF2(float _initial, float _final){
         return (_final - _initial) * (f2((_final + _initial) / 2));
     }
     
+    //Calculate the defined integral from _initial to _final for function 2 using Trapezoidal method
     float trapezoidalIntegrationF2(float _initial, float _final){
         return (_final - _initial) * ((f2(_initial) + f2(_final)) / 2);
     }
     
+    //Calculate the defined integral from _initial to _final for function 2 using Simpson method
     float simpsonIntegrationF2(float _initial, float _final){
         return (_final - _initial) * ((f2(_initial) + (4* (f2((_final + _initial) / 2)) ) +  f2(_final)) / 6);
     }
     
     //F3
+    //Calculate the defined integral from _initial to _final for function 3 using MidPoint method
     float midPointIntegrationF3(float _initial, float _final){
         return (_final - _initial) * (f3((_final + _initial) / 2));
     }
     
+    //Calculate the defined integral from _initial to _final for function 3 using Trapezoidal method
     float trapezoidalIntegrationF3(float _initial, float _final){
         return (_final - _initial) * ((f3(_initial) + f3(_final)) / 2);
     }
     
+    //Calculate the defined integral from _initial to _final for function 3 using Simpson method
     float simpsonIntegrationF3(float _initial, float _final){
         return (_final - _initial) * ((f3(_initial) + (4* (f3((_final + _initial) / 2)) ) +  f3(_final)) / 6);
     }
     
     //Adaptative
+    //Calculates the integral of f1 using mid point method and adaptative squares
     float midPointIntegrationF1(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -92,6 +109,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += midPointIntegrationF1(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -99,13 +117,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f2 using mid point method and adaptative squares
     float midPointIntegrationF2(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -114,6 +135,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += midPointIntegrationF2(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -121,13 +143,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f3 using mid point method and adaptative squares
     float midPointIntegrationF3(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -136,6 +161,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += midPointIntegrationF3(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -143,13 +169,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f1 using trapezoidal method and adaptative squares
     float trapezoidalIntegrationF1(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -158,6 +187,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += trapezoidalIntegrationF1(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -165,13 +195,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f2 using trapezoidal method and adaptative squares
     float trapezoidalIntegrationF2(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -180,6 +213,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += trapezoidalIntegrationF2(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -187,13 +221,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f3 using trapezoidal method and adaptative squares
     float trapezoidalIntegrationF3(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -202,6 +239,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += trapezoidalIntegrationF3(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -209,13 +247,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f1 using simpson method and adaptative squares
     float simpsonIntegrationF1(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -224,6 +265,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += simpsonIntegrationF1(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -231,13 +273,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f2 using simpson method and adaptative squares
     float simpsonIntegrationF2(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -246,6 +291,7 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += simpsonIntegrationF2(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
@@ -253,13 +299,16 @@ public:
         return result;
     }
     
+    //Calculates the integral of f3 using simpson method and adaptative squares
     float simpsonIntegrationF3(float _initial, float _final, int n_squares){
         
+        //Define interval between squares
         float interval = (_final - _initial) / n_squares;
         float temp = _initial;
         TemplateLDE<float> integration_intervals;
         float result = 0;
         
+        //Create a list with the intervals
         integration_intervals.insertNodeFront(_initial);
         while (temp != _final) {
             temp = temp + interval;
@@ -268,193 +317,12 @@ public:
         }
         integration_intervals.removeFrontNode();
         
+        //Calculate the integral with all the intervals from the list
         do {
             result += simpsonIntegrationF3(integration_intervals.removeBackNode(), integration_intervals.removeBackNode());
         } while (!integration_intervals.isEmpty());
         
         return result;
-    }
-    
-    //Errors
-    
-    float midPointErrorF1(int n_squares, float a, float b){
-        
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 2;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i < order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f1(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = (pow(h, 3)/24)*derivate;
-        
-        return result;
-    }
-    
-    float midPointErrorF2(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 2;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f2(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = (pow(h, 3)/24)*derivate;
-        
-        return result;
-    }
-    
-    float midPointErrorF3(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 2;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f3(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = (pow(h, 3)/24)*derivate;
-        
-        return result;
-    }
-    
-    float trapezoidalErrorF1(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 2;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f1(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = -(pow(b - a, 3)/12)*derivate;
-        
-        return result;
-    }
-    
-    float trapezoidalErrorF2(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 2;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f2(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = -(pow(b - a, 3)/12)*derivate;
-        
-        return result;
-    }
-    
-    float trapezoidalErrorF3(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 2;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f3(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = -(pow(b - a, 3)/12)*derivate;
-        
-        return result;
-    }
-    
-    float simpsonErrorF1(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 4;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f1(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = -(pow(b - a, 5)/2880)*derivate;
-        
-        return result;
-    }
-    
-    float simpsonErrorF2(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 4;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f2(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = -(pow(b - a, 5)/2880)*derivate;
-        
-        return result;
-    }
-    
-    float simpsonErrorF3(int n_squares, float a, float b){
-        float result = 0;
-        float h = (b-a)/n_squares;
-        int order = 4;
-        float small_number = 0.00000001;
-        
-        float derivate = 0;
-        float sum = 0;
-        for (int i = 0; i <= order; i++) {
-            sum += pow(-1, i+order) * comb(order, i) * f3(h + (i* small_number));
-        }
-        
-        derivate = (1/(pow(small_number, order))) * sum;
-        
-        result = -(pow(b - a, 5)/2880)*derivate;
-        
-        return result;
-    }
-    
-    float adaptativeMidPointF1(float _initial, float _final, float error){
-        int n_squares = 1;
-        while (abs(midPointErrorF1(n_squares, _initial, _final)) > error) {
-            n_squares++;
-        }
-        return midPointIntegrationF1(_initial, _final, n_squares);
     }
     
 };
--- libs/Aula6/MonteCarlo.hpp
+++ libs/Aula6/MonteCarlo.hpp
@@ -17,31 +17,40 @@ class MonteCarlo {
 
 public:
     
+    //Evaluates the equation on point x
     float Equation(float x){
         return 4 / (1 + pow(x, 2));
     }
     
+    //Integrate equation defined between the from and to variables
     float IntegrateEquation(float from, float to, int attempts){
         
         float average = 0;
         
+        //Defines a random generator
         std::random_device rd;
         std::mt19937 mt(rd());
+        
+        //Defines boundries of random numbers to be generated
         std::uniform_real_distribution<double> dist(from, to);
         
+        //Evaluates the equation with random numbers as many times as defined in the variable attempts
         for (int i = 0; i < attempts; i++) {
             average += this->Equation(dist(mt));
         }
         
+        //Calculates the average of evaluations
         average = average / attempts;
         
         return (to - from) * average;
     }
     
+    //Evaluate the toroidal equation on points x, y, z
     float Toroide(float x, float y, float z){
         return pow(z, 2) + pow(( sqrt(pow(x, 2) + pow(y, 2) ) - 3 ), 2) <= 1 ? 1 : 0;
     }
     
+    //Integrate the toroidal equation finding the volume of the figure
     double IntegrateToroide( double x_from,
                   double x_to,
                   double y_from,
@@ -50,28 +59,42 @@ public:
                   double z_to,
                   int attempts
             ){
+        //Defines a random generator
         std::random_device rd;
         std::mt19937 mt(rd());
+        
+        //Defines boundries of random numbers to be generated on x
         std::uniform_real_distribution<double> dist_x(x_from, x_to);
+        //Defines boundries of random numbers to be generated on y
         std::uniform_real_distribution<double> dist_y(y_from, y_to);
+        //Defines boundries of random numbers to be generated on z
         std::uniform_real_distribution<double> dist_z(z_from, z_to);
         
         double average = 0;
         double n = 0;
-        double cube_volume = (x_to - x_from) * (y_to - y_from) * (z_to - z_from);
         double x = 0;
         double y = 0;
         double z = 0;
         int count = 0;
         
+        //Calculates the volume of the cube that involves the figure
+        double cube_volume = (x_to - x_from) * (y_to - y_from) * (z_to - z_from);
+        
+        //Loop to evaluate the equation with random numbers
         while (count < attempts){
             
+            //Find a random number for x
             x = dist_x(mt);
+            //Find a random number for y
             y = dist_y(mt);
+            //Find a random number for z
             z = dist_z(mt);
             
+            //Evaluate the equation with random numbers
             n = Toroide(x, y, z);
             
+            //Check if the point evaluated is inside the figure
+            //if so, add one to averge variable
             if (n <= 1 && x > 1 && y >= -3 ) {
                 average += n;
                 count++;
@@ -79,13 +102,16 @@ public:
             
         }
         
+        //Return the value of the figure volume
         return cube_volume * (average / attempts);
     }
     
+    //Execute the equation integration with monte carlo method
     void test_integrate_equation(){
         std::cout<< "Equation Integral - > " << IntegrateEquation(0, 1, 1000000)<< std::endl;
     }
     
+    //Execute the toroidal equation integration with monte carlo method
     void test_integrate_toroide(){
         std::cout<< "Toroide Integral - > " << IntegrateToroide(1, 4, -3, 4, -1, 1, 100000000)<< std::endl;
     }
